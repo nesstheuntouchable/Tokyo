2da91251ca3edd93a159487c9328c66d090d86dc
nesstheuntouchable <145471779+nesstheuntouchable@users.noreply.github.com>
2023-11-09 12:09:54 -0500

    Add files via upload

---
+++ 2-Subnets.tf
@@ -0,0 +1,85 @@
+#These are   for  public
+
+resource "aws_subnet" "public-ap-northeast-1a" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.40.1.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-ap-northeast-1a"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
+
+resource "aws_subnet" "public-ap-northeast-1c" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.40.3.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-ap-northeast-1c"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
+
+
+resource "aws_subnet" "public-ap-northeast-1d" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.40.4.0/24"
+  availability_zone       = "ap-northeast-1d"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-ap-northeast-1d"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
+
+#these are for private
+resource "aws_subnet" "private-ap-northeast-1a" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.40.11.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name    = "private-ap-northeast-1a"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
+
+resource "aws_subnet" "private-ap-northeast-1c" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.40.13.0/24"
+  availability_zone = "ap-northeast-1c"
+
+  tags = {
+    Name    = "private-ap-northeast-1c"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
+
+
+resource "aws_subnet" "private-ap-northeast-1d" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.40.14.0/24"
+  availability_zone = "ap-northeast-1d"
+
+  tags = {
+    Name    = "private-ap-northeast-1d"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
